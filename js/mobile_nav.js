d73ae27ff6611acfa2eaf2eb49a30f20cc07b32a
Eason <eason.pan@intel.com>
2019-01-08 21:31:28 +0800

    read

--- js/mobile_nav.js
+++ js/mobile_nav.js
@@ -3,21 +3,28 @@
 
   var body = document.getElementsByTagName('body')[0];
   var navToggle = document.getElementById('mobile-nav-toggle');
-  var container = document.getElementById('container');
-  var dimmer = document.getElementById('mobile-nav-dimmer');
+  var container = document.getElementById('container'),
+      collapse = document.querySelector(".navbar-collapse")
+  // var dimmer = document.getElementById('mobile-nav-dimmer');
   var CLASS_NAME = 'mobile-nav-on';
   if (!navToggle) return;
 
   navToggle.addEventListener('click', function(e){
     e.preventDefault();
     e.stopPropagation();
-    body.classList.toggle(CLASS_NAME);
+    if(document.querySelector(".navbar-collapse").className.indexOf('on') < 0) {
+      collapse.className = "navbar-collapse on"
+    } else {
+      collapse.className = "navbar-collapse"
+    }
+    
+    //body.classList.toggle(CLASS_NAME);
   });
 
-  dimmer.addEventListener('click', function(e){
-    if (!body.classList.contains(CLASS_NAME)) return;
+  // dimmer.addEventListener('click', function(e){
+  //   if (!body.classList.contains(CLASS_NAME)) return;
 
-    e.preventDefault();
-    body.classList.remove(CLASS_NAME);
-  });
+  //   e.preventDefault();
+  //   body.classList.remove(CLASS_NAME);
+  // });
 })();
